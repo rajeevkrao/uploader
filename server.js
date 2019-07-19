e1592c95249f66f9f61331d9a93f9781fff788ed
Glitch (ramer) <none>
2019-07-19 15:06:11 +0000

    🐀🌇 Checkpoint ./server.js:610302/608 ./package.json:610302/3416

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // init project
 var express = require('express');
 var app = express();
+var formidable = require('formidable');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -12,7 +13,31 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + '/app/index.html');
 });
 
+app.get("/upload", function(request, response) {
+  response.sendFile(__dirname + '/app/index.html');
+});
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res){
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/uploads/' + file.name;
+    });
+
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
+    });
+
+    res.sendFile(__dirname + '/index.html');
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);  
 });
