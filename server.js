ff679bf5e803a57292314e49b9884dbdec1b65a3
Glitch (ramer) <none>
2019-07-22 12:05:28 +0000

    🎳🌨 Checkpoint ./server.js:610302/115 ./app/components/HelloWorld.jsx:610302/262

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.get('/getlist', function (req, res){
   const folder = path.join(__dirname, '/app/uploads/');
   
   var files = fs.readdir(folder, (err, files) => {
+    var result = JSON.stringify(files);
+    res.send(result);
+
   files.forEach(file => {
     console.log(file);
   });
