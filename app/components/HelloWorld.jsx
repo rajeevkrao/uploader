0ef10fa5dbdef6f33a81b1b217c443a41a766801
Glitch (ramer) <none>
2019-07-27 17:34:49 +0000

    🏦👮 Checkpoint ./app/components/HelloWorld.jsx:610302/1215

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -35,10 +35,6 @@ class App extends Component {
     
     
     var { isLoaded, items } = this.state;
-    var data,edata;
-    items.forEach(function(element) {
-      edata=<li>{element}</li>
-    });
     if(!isLoaded) {
        return <div>Loading...</div>
     }
@@ -46,64 +42,11 @@ class App extends Component {
     else{
       return(
         <div className="App">
-          Data has been loaded...
+          
         </div>
       );
     }
-    
-    return (
-        <div className="App">
-      
-      </div>
-    );
   }
 }
 
 module.exports = App;
-
-
-/*
-const axios = require('axios');
-
-const http = require('http');
-
-
-var car=fetch('https://ramer.glitch.me/getlist')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
-
-//const list= require('../../list.json');
-
-//const transfer=require('./test.js');
-
-var dat;
-
-var b=http.get('https://ramer.glitch.me/getlist', (res) => {
-  var a=res.on('data', function (chunk) {
-      chunk = JSON.parse(chunk);
-          return chunk;
-  });
-  return a;
-  
-});
-
-
-var data=<div>  
-  <p>
-    
-  </p>
-  </div>
-
-const HelloWorld = function() {
-  return (data
-  );
-}
-
-module.exports = HelloWorld;
-*/
-
-
